8131596c67ed7aeae0aedf44b9a36b17bf7601a7
vladborichevsky <mr.vladinho@mail.ru>
2022-02-05 21:39:32 +0300

    'Have added section catalog and mobile version for it. Changed dots in section carousel for mobile version'

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"></button>',
         responsive: [
             {
-              breakpoint: 992,
+              breakpoint: 800,
               settings: {
                 dots: true,
                 arrows: false,
@@ -16,4 +16,26 @@ $(document).ready(function(){
             }
           ]
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    $('.catalog-item__link').each(function(i) {
+      $(this).on('click', function(e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__more').eq(i).toggleClass('catalog-item__more_active');
+      })
+    })
   });
+
+    $('.catalog-item__back').each(function(i) {
+      $(this).on('click', function(e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__more').eq(i).toggleClass('catalog-item__more_active');
+      })
+    });
